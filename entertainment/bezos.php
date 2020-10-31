7804afa5a303d30406651852f2b148861c036582
prabhushakti <pharidaswa@gmail.com>
2020-10-31 09:39:38 +0530

    weodia-alpha

---
+++ entertainment/bezos.php
@@ -0,0 +1,244 @@
+<!-- 
+	(c)WeOdia.com
+	Currenty we are looking for volunteers (web developers, content writers). 
+	If you have the any such skills, please email hi@weodia.com
+-->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="robots" content="index, follow" />
+		<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+		<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+		<meta name="DuckDuckBot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+		<!-- <link rel="canonical" href="https://weodia.com/electronics/bass_treble_in_odia.html" /> -->
+
+		<title>
+		ବିଶ୍ଵର ସବୁଠାରୁ ଧନୀ - ବେଜସ  | "WeOdia: Everyting Odia!" | Odisha's First Infotainment Website | ଇଲେକ୍ଟ୍ରୋନିକ୍ସ | କିଣାକିଣି | ବିଜ୍ଞାନ | ଶିକ୍ଷା | ମନୋରଂଜନ | ରୋଷେଇ |
+		</title>
+		<meta property="og:title" content="ବିଶ୍ଵର ସବୁଠାରୁ ଧନୀ - ବେଜସ (Jeff Bezos)| WeOdia: Everyting Odia! |  First Infotainment Website of Orissa | ଇଲେକ୍ଟ୍ରୋନିକ୍ସ | କିଣାକିଣି | ବିଜ୍ଞାନ | ଶିକ୍ଷା | ମନୋରଂଜନ | ରୋଷେଇ |" />
+		<meta name="description" content="About Jeff Bezons in Odia" />
+		<meta property="og:description" content="About Jeff Bezons in Odia" />
+		<? include "ent-header.html"; ?>
+			<div id="post-header" class="page-header">
+				<div class="background-img" style="background-image: url('../img/entertainment/money.jpg');"></div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-10">
+							<div class="post-meta">
+								<a class="post-category cat-4" href="entertainment.html">ମନୋରଞ୍ଜନ</a>
+								<span class="post-date"> originialy written in June, published Aug 25</span>
+							</div>
+							<h1>ବିଶ୍ଵର ସବୁଠୁ ଧନୀ ଜେଫଙ୍କ ବିଷୟରେ ପାଞ୍ଚଟି କଥା </h1>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /Page Header -->
+		</header>
+		<!-- /Header -->
+
+		<!-- section -->
+		<div class="section noselect">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<!-- Post content -->
+					<div class="col-md-8">
+						<div class="section-row sticky-container">
+							<div class="main-post">
+                                <ul>
+                                    
+                                        <figure class="figure-img">
+                                            <img class="img-responsive" src="../img/entertainment/jeff-bezos-amazon.jpg" alt="Bezos at Amazon">
+                                        <figcaption>&copy;TOMMASO BODDI</figcaption>
+                                        </figure>
+                                        <li><p>
+                                        ଜେଫ ବେଜୋସ 1964ରେ ଜନ୍ମ ଗ୍ରହଣ କରିଥିଲେ ଜିଏକି ବିଶ୍ଵର  ସବୁଠାରୁ ଧନୀ ବ୍ୟକ୍ତି ତାଙ୍କର ସମ୍ପତିର ମୂଲ୍ୟ ପ୍ରାୟ ୧୪ ଲକ୍ଷ ପଚାଶ ହଜାର କୋଟି ଟଙ୍କା। 
+                                        ସେ ବିଶ୍ଵର ଦ୍ୱିତୀୟ ଧନୀ ବିଲ ଗେଟ୍ସଙ୍କ ଠାରୁ ପ୍ରାୟ ଦୁଇଗୁଣ ଅଧିକ ଧନର ମାଲିକ ।</p> 
+                                    </li>
+                                    <li>
+                                        <p>
+                                            ଗୁଗଲ କମ୍ପାନୀ ଆରମ୍ଭରୁ ସେ ସେଥିରେ ୩୦ କୋଟି ଟଙ୍କାର ସେଆର ଭାବେ ପୁଞ୍ଜି ନିବେଶ କରିଥିଲେ, ଯାହା ଆଜି ପାଖାପାଖି ପଚିଶ ହଜାର କୋଟିରେ ପହଞ୍ଚିଛି । 
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            ତାଙ୍କ ସ୍ତ୍ରୀଙ୍କୁ ଛାଡପତ୍ର ଦେବାରୁ ଜେଫଙ୍କୁ ଦୁଇ ଲକ୍ଷ ସତଷଠି ହଜାର ଟଙ୍କା ଦେବାକୁ ପଡିଥିଲା ଯାହା ପାଇଁ ତାଙ୍କ ସ୍ତ୍ରୀ ବିଶ୍ଵର ତୃତୀୟ ଧନୀ ମହିଳା ହୋଇପାରିଲେ । 
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            ନିଜର କମ୍ପାନୀ ଆମାଜନରେ ତାଙ୍କର ଦରମା ଏକ ସାଧାରଣ କର୍ମଚାରୀର ବେତନ ହେଲେ ମଧ୍ୟ ଅନ୍ୟାନ୍ୟ ବ୍ୟବସାୟରୁ ପ୍ରତି ସେକେଣ୍ଡକୁ ଅଢେଇ ଲକ୍ଷ ଟଙ୍କା ରୋଜଗାର କରନ୍ତି । 
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            ଯଦି ଆପଣ ଆମାଜନ କମ୍ପାନୀର ସେବାରେ ଅସନ୍ତୁଷ୍ଟ ତେବେ ତାଙ୍କୁ ସିଧାସଳଖ ଇମେଲ କରିପାରିବେ, ତାଙ୍କ ଇମେଲ ଆଡ୍ରେସ ହେଲା: jeff@amazon.com    
+                                        </p>
+                                    </li>
+                                </ul>
+								
+                                    
+                                    
+							</div>
+					    </div>
+
+						
+						
+						
+					</div>
+				
+
+					<div class="col-md-4">
+						
+
+					
+						<div class="aside-widget">
+							<div class="section-title">
+								<h2>ନିଶ୍ଚୟ ପଢନ୍ତୁ</h2>
+							</div>
+
+							<div class="post post-widget">
+								<a class="post-img" href="../electronics/usb_details.html"><img src="../img/how/USB_plug.svg" alt="usb vetor image"></a>
+								<div class="post-body">
+									<h3 class="post-title"><a href="../electronics/usb_details.html">ୟୁଏସବି ସମ୍ବଦ୍ଧରେ ବିସ୍ତୃତରେ ପଢନ୍ତୁ</a></h3>
+								</div>
+                            </div>
+                  
+							<div class="post post-widget">
+								<a class="post-img" href="../electronics/how_to_buy_smartphone.html"><img src="../img/how/Mobile-Phone.svg" alt="a phone image"></a>
+								<div class="post-body">
+									<h3 class="post-title"><a href="../electronics/how_to_buy_smartphone.html">ସଠିକ ମୋବାଇଲ ଫୋନ କିଣିବେ କିପରି? ପଢନ୍ତୁ ଆମର ବିସ୍ତୃତତ୍ ଲେଖା </a></h3>
+								</div>
+							</div>
+							<div class="post post-widget">
+								<a class="post-img" href="../science/how_tv_works.html"><img src="../img/how/CRT_monitor.svg" alt="a crt tv monitor vector image"></a>
+								<div class="post-body">
+									<h3 class="post-title"><a href="../science/how_tv_works.html">ଟିଭି ଚାଲେ କିପରି??</a></h3>
+								</div>
+							</div>
+						</div>
+					
+						<div class="aside-widget">
+							<div class="section-title">
+								<h2>ପ୍ରମୁଖ ଲେଖା</h2>
+							</div>
+							<div class="post post-thumb">
+								<a class="post-img" href="../biography/the_great_dictator_final_speech_in_odia.html"><img src="../img/entertainment/chaplin_in_the_great_dictator.jpg" alt="Charlie Chaplin in kid film copyright by the chaplin archived"></a>
+								<div class="post-body">
+									<div class="post-meta">
+										<a class="post-category cat-1" href="../history.html">ଇତିହାସ </a>
+										<span class="post-date">Aug 27, 2020</span>
+									</div>
+									<h3 class="post-title"><a href="../biography/the_great_dictator_final_speech_in_odia.html">ଚାର୍ଲି ଚପଲିନଙ୍କ ବିଶ୍ଵ ପ୍ରସିଦ୍ଧ ଭାଷଣ ପଢନ୍ତୁ </a></h3>
+								</div>
+							</div>
+
+							<div class="post post-thumb">
+								<a class="post-img" href="../science/continents.html"><img src="../img/geography/holding-earth.jpg" alt="continets of the world"></a>
+								<div class="post-body">
+									<div class="post-meta">
+										<a class="post-category cat-1" href="#">ଇତିହାସ </a>
+										<span class="post-date">Aug 28, 2020</span>
+									</div>
+									<h3 class="post-title"><a href="../science/continents.html">ମହାଦେଶ ବିଷୟରେ ଜାଣନ୍ତୁ କିଛି ତଥ୍ୟ </a></h3>
+								</div>
+							</div>
+						</div>
+						<div class="class section aside-widget">
+							<div class="section-title category-widget">
+								<h2>&ThickSpace;&ThickSpace;ପସନ୍ଦର ବିଷୟବସ୍ତୁ ପଢନ୍ତୁ</h2> 
+							</div>
+						</div>
+						<div class="aside-widget">
+							<div class="tags-widget">
+								<ul>
+									<li><a href="../entertainment.html">ମନୋରଞ୍ଜନ</a></li>
+									<li><a href="../science.html">ଜ୍ଞାନବିଜ୍ଞାନ</a></li>
+									<li><a href="../electronics.html">ଇଲେକ୍ଟ୍ରୋନିକ</a></li>
+									<li><a href="../history.html">ଇତିହାସ</a></li>
+									<li><a href="../archives.html">ଅଭିଲେଖା</a></li>
+									<li><a href="../electronics/how_to_buy_smartphone.html">ସ୍ମାର୍ଟଫୋନ</a></li>
+									<li><a href="../electronics/computer_science_basics_in_odia.html">କଂପ୍ୟୁଟର</a></li>
+									<li><a href="../history.html">ମଜାକଥା</a></li>
+									<li><a href="../quotes.html">ନୀତିବାଣୀ</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="">
+									<h1 style="font-family: Kalinga;" class="">WeOdia.com</h1>
+									<ul class="">
+										<li><a href="../about-us.html">ଆମ୍ଭ ବିଷୟରେ</a></li>
+										<li><a href="../about-us.html">ଆମ ସହିତ କାମ କରନ୍ତୁ</a></li>
+										<li><a href="../about-us.html">ଯୋଗାଯୋଗ</a></li>
+										<li><a href="../privacy.html">ଗୋପନୀୟତା ଚୁକ୍ତି (Privacy Policy)</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+<br>
+					<div class="col-md-3">
+						<div class="footer-widget">
+							<h2 style="font-family: Kalinga"> ସୋସିଆଲ ସାଇଟ</h2>
+							<ul class="">
+							<li><a href="https://facebook.com/weoriya"><img style="height:28px" src="../img/gif/facebook-like-morph.gif"></i></a>&ThickSpace;<a style="color:navy" href=https://facebook.com/weoriya>facebook.com/<b>WeOriya</b></a></li>
+							<li><a href="https://twitter.com/weodia"><i class="fa fa-twitter fa-spin" style="font-size:30px; color:#1DA1F2"></i></a><a style="color:#1da1f2" href=https://twitter.com/WeOdia>@WeOdia</a></li>
+							<li><a href="https://instagram.com/weodia"><img style="height:30px" src="../img/gif/instagram-shot.gif"></i></a>&ThickSpace;<a style="color:navy;font-size: 15px;" href=https://instagram.com/WeOriya>instagram.com/<b>WeOriya</b></a></li>
+							<li><a href="https://pinterest.com/weoriya"><i class="fa fa-pinterest fa-spin" style="font-size:30px; color:red"></i></a>&ThickSpace;<a style="color:red" href=https://pinterest.com/weoriya>pinterest.com/WeOriya</a></li>
+							<li><a href="https://youtube.com/channel/UCAUWUTkqoq4qSqkW92MqOOQ"><i class="fa fa-youtube-play" style="font-size:30px; color:red"></i></a>&ThickSpace;<a style="color:red" href=https://www.youtube.com/channel/UCAUWUTkqoq4qSqkW92MqOOQ>Visit our Channel </a></li>
+						</ul> 
+
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div id="slideshow">
+							<div>Thank you for visiting our website. ଅଶେଷ ଧନ୍ୟବାଦ ୱେବ୍ସାଇଟକୁ ଆସିଥିବାରୁ । <img src="../img/gif/thank-you-angel-animated.gif">
+							</div>
+							<div><img src="../img/logo(6).png">Everything Odia!</div>
+
+							<div><img src="../img/history/ 1914_danzin.png">An advertisement in 1914 (୧୯୧୪ ମସିହାର ଏକ ବିଜ୍ଞାପନ) </div>
+							<div>ଆମର ଇତିହାସ ପେଜ୍ ରେ ପାଇବେ ଶତାବ୍ଦୀ ପୂର୍ବ ଓଡ଼ିଶାର ଅନେକ ଐତିହାସିକ ଚିତ୍ର, ଲେଖା ଇତ୍ୟାଦି ।</div>
+							<div>ମୁଁ ଯେଉଁଟା କାମ୍ୟ ବୋଲି ବିଶ୍ଵାସ କରୁଛି, ସଂସାରର ପରିବର୍ତ୍ତନ ଲାଗି ଯେଉଁସବୁ ମୂଳତମ ମୂଲ୍ୟକୁ ମୁଁ ଅପରିହାର୍ଯ୍ୟ ବୋଲି ଅନୁଭବ କରୁଛି, ମୁଁ ତାହାରି ଅନୁସାରେ ମୋ ଜୀବନ ଟାକୁ ବଞ୍ଚିବି, ସକଳ ପ୍ରତିକୂଳତା ସତ୍ତ୍ବେ ମୁଁ ନିଜର ଜୀବନକୁ ତାହାରି ଧାରାରେ ନେଇ ପକାଇବାକୁ ଇଛା କରିବି ଓ ପ୍ରୟାସ କରିବି -
+								ଆପଣା ପାଖରେ ଆପେ ଜଣେ ମଣିଷବୋଲି ସ୍ବୀକୃତ ହୋଇପାରିବାଲାଗି ତାହାହିଁ ପ୍ରତ୍ୟକ ମନୁଷ୍ୟଠାରୁ ପ୍ରତ୍ୟାଶା କରାଯାଉଥିବା ସର୍ବନିମ୍ନ ସାଧୁତା । - ଚିତ୍ତରଞ୍ଜନ ଦାସ </div> 
+							<div>ଆହୁରି ନୀତିବାଣୀ ପାଇଁ ଆମ ନୀତିବାଣୀ ପେଜ୍ କୁ ଯାନ୍ତୁ  </div>
+							<div>ଆପଣ ଜାଣିଛନ୍ତି କି ଅଲମ୍ପିକ ଖେଳର ପ୍ରତୀକ କିମ୍ବା ପତାକାରେ କେବଳ କଳା, ନାଲି, ହଳଦିଆ, ନୀଳ ଓ ସବୁଜ ବର୍ଣ୍ଣ କାହିଁକି ଥାଏ?
+								କାରଣ ବିଶ୍ଵର ଯେ କୌଣସି ବି ଦେଶର ପତାକାରେ ଏହି ବର୍ଣ୍ଣ ମଧ୍ୟରୁ ଗୋଟିଏ ବର୍ଣ୍ଣ ନିଚୟ ରହିଥାଏ ।   
+							</div>
+							<div>ଚିଲି ଦେଶର କଲମା ନାମକ ସହରରେ କେବେବି ବର୍ଷା ହୁଏନି ।</div>
+							<div>ଲାଇଟର ଓ ଡିଆସିଲିର ମଧ୍ୟରେ କେଉଁଟି ପ୍ରଥମେ ଉଦ୍ଭାବନ ହୋଇଥିଲା?<br> ଉତ୍ତର: ଲାଇଟର!</div>
+							<div>1865, ଫେବୃଆରୀ ମାସରେ ପୂର୍ଣ୍ଣିମା ହୋଇନଥିଲା ।</div>
+							<div>କେଉଁଟି ଆଗ ଆସିଲା - କୁକୁଡ଼ା ନା ଅଣ୍ଡା? ବାଇବେଲ କହେ କୁକୁଡ଼ା !</div>
+							<div>ମଣିଷ ଯେତେ ଚେଷ୍ଟା କଲେବି ନିଜ କହୁଣିକୁ ଜିଭ ଦ୍ଵାରା ଛୁଇଁ ପାରେନି!</div>
+							<div>ଆପଣଙ୍କ ଛିଙ୍କର ବେଗ 162 କିଲୋମିଟର ପ୍ରତି ଘଣ୍ଟା ହୋଇଥାଏ?</div>
+							<div>ପ୍ରଜାପତି କେମିତି ସ୍ବାଦର ଅନୁଭବ କରିଥାଏ? <br>ଉତ୍ତର: ନିଜ ପାଦ ଦ୍ଵାରା!</div>
+							<div>ଗେଣ୍ଡା କେତେ ସମୟ ପର୍ଯ୍ୟନ୍ତ ଶୋଇପାରେ? <br>ଉତ୍ତର: ତିନି ବର୍ଷ!</div>
+							<div>ହ୍ୱେଲ ମାଛର ଜିଭର ଓଜନ ଏକ ହାତୀ ତୁଳନାରେ ଅଧିକ!</div>
+							<div>ପୁରାତନ ଇଜିପ୍ଟ ନିବାସୀ ମାନେ ପଥରରେ ତିଆରି ତକିଆରେ ଶୟନ କରୁଥିଲେ!</div>
+							<div>ଇଂରାଜୀ ଅକ୍ଷର ଆଇ (i) ଉପରେଥିବା ବିନ୍ଦୁକୁ ଟିଟିଲ କହନ୍ତି!</div>
+							<div>କୋକା-କୋଲାର ପ୍ରଥମେ ସବୁଜ ରଙ୍ଗରେ ଆସିଥିଲା!</div>
+							<div><a href="../history.html"><img src="../img/history/1921_usa-anti-racism.png">ଅଧିକ ପଢିବା ପାଇଁ କ୍ଲିକ କରନ୍ତୁ </a></div>
+						</div>
+					</div>
+ 				</div>
+			</div>
+		</footer>
+		<script src="../js/jquery.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/main.js"></script>
+	</body>
+</html>
